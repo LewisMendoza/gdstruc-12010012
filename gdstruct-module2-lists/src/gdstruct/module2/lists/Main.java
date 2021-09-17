7c0b279fe49f496c3ad4587c1c14f68c7e38d894
LEWI-PC\LewiLewi <lewisgilbert.mendoza@benilde.edu.ph>
2021-09-17 18:01:25 +0800

    Added sample module 2 lists

---
+++ gdstruct-module2-lists/src/gdstruct/module2/lists/Main.java
@@ -0,0 +1,39 @@
+package gdstruct.module2.lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Player asuna = new Player(1, "Asuna", 100);
+        Player lethalBacon = new Player(2, "LethalBacon", 205);
+        Player hpDeskJet = new Player(3, "HPDeskjet", 34 );
+
+        PlayerLinkedList playerLinkedList = new PlayerLinkedList();
+
+        playerLinkedList.addToFront(asuna);
+        playerLinkedList.addToFront(lethalBacon);
+        playerLinkedList.addToFront(hpDeskJet);
+
+        playerLinkedList.printList();
+
+        // PART 1
+        // Access an element
+        // System.out.println(playerList.get(1));
+
+       // playerList.add(2, new Player(553, "Arctis", 55));
+
+      // playerList.remove(2);
+
+        //System.out.println(playerList.contains(new Player(2, "LethalBacon", 205)));
+
+      //  System.out.println(playerList.indexOf(new Player(2, "LethalBacon", 205)));
+
+        // Java 8 forEach
+        // playerList.forEach(player -> System.out.println(player));
+
+//        for (Player p : playerList)
+//      {
+//           System.out.println(p);
+//      }
+   }
+}
---
+++ untitled/src/gdstruct/module2/lists/Main.java
@@ -0,0 +1,8 @@
+package gdstruct.module2.lists;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
